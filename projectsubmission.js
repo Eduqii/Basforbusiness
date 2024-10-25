c81736c227a18901c4647994d070c98fb71f4e50
Bernard Osei Dwamena <77701940+Eduqii@users.noreply.github.com>
2024-10-25 17:57:17 +0000

    Update projectsubmission.js

--- projectsubmission.js
+++ projectsubmission.js
@@ -1,1 +1,5 @@
-
+const loadpage = function(){
+let loadr = document.querySelector("#loadr");
+  loadr.innerHTML = "<span class='spinner-border bg-dark'></span>";
+  document.write("<h1 class='display-1'>Let us help you get your business online. " + "<a href='https://www.google.com?search/text=benit%20apollo%20solutions'>Find us online </a></h1>");
+}
